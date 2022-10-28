09e7f7db34b323439c2bda40b76d582377450732
mellanie <mellanie.achieng@gmail.com>
2022-10-28 13:18:37 +0000

    1. Cash only

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -10,7 +10,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
@@ -36,7 +35,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
